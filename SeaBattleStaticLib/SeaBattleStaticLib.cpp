9ea0ef82fe42ede10efa6f7663f5e41d69de99a7
Новиков Роман <roman141aleksandro2007@gmail.com>
2026-09-22 21:45:08 +0300

    Добавлена реализация класса Position  и тесты к ней

--- SeaBattleStaticLib/SeaBattleStaticLib.cpp
+++ SeaBattleStaticLib/SeaBattleStaticLib.cpp
@@ -3,8 +3,66 @@
 
 #include "pch.h"
 #include "framework.h"
+#include "main.h"
 
-// TODO: Это пример библиотечной функции.
-void fnSeaBattleStaticLib()
-{
+
+Position::Position(int row, int col) : _row(row), _col(col) {
+    if (row < 1 || row > _max_row || col < 1 || col > _max_col) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+}
+Position::Position(const std::string& str) {
+    int row, col;
+    row = 1;
+    col = 1;
+    std::istringstream iss(str);
+    char ch1, ch2, ch3;
+
+    iss >> ch1 >> row >> ch2 >> col >> ch3;
+
+    if (ch1 != '(' || ch2 != ',' || ch3 != ')' || iss.fail()) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+
+    if (row < 1 || row > _max_row || col < 1 || col > _max_col) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+
+    _row = row;
+    _col = col;
+}
+void Position::row(int new_row) {
+    if (new_row < 1 || new_row > _max_row) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+    _row = new_row;
+}
+void Position::col(int new_col) {
+    if (new_col < 1 || new_col > _max_col) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+    _col = new_col;
+}
+std::string to_string(const Position& pos) {
+    std::ostringstream oss;
+    oss << "(" << pos._row << ", " << pos._col << ")";
+    return oss.str();
+}
+
+Position parse(const std::string& str) {
+    std::istringstream iss(str);
+    char ch1, ch2, ch3;
+    int row, col;
+
+    iss >> ch1 >> row >> ch2 >> col >> ch3;
+
+    if (ch1 != '(' || ch2 != ',' || ch3 != ')' || iss.fail()) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+
+    if (row < 1 || row > Position::_max_row || col < 1 || col > Position::_max_col) {
+        throw std::logic_error("Invalid input: incorrect position");
+    }
+
+    return Position(row, col);
 }
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,186 @@
 #include "pch.h"
+#include "SeaBattleStaticLib.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+//class Position
+TEST(PositionTest, DefaultConstructor) {
+    Position pos;
+    EXPECT_EQ(pos.row(), 1);
+    EXPECT_EQ(pos.col(), 1);
+}
+
+TEST(PositionTest, CopyConstructor) {
+    Position pos1(5, 7);
+    Position pos2(pos1);
+    EXPECT_EQ(pos2.row(), 5);
+    EXPECT_EQ(pos2.col(), 7);
+}
+
+TEST(PositionTest, ValidIntConstructor) {
+    Position pos(3, 8);
+    EXPECT_EQ(pos.row(), 3);
+    EXPECT_EQ(pos.col(), 8);
+}
+
+TEST(PositionTest, IntConstructorMinBounds) {
+    Position pos(1, 1);
+    EXPECT_EQ(pos.row(), 1);
+    EXPECT_EQ(pos.col(), 1);
+}
+
+TEST(PositionTest, IntConstructorMaxBounds) {
+    Position pos(10, 10);
+    EXPECT_EQ(pos.row(), 10);
+    EXPECT_EQ(pos.col(), 10);
+}
+
+TEST(PositionTest, IntConstructorRowTooLow) {
+    EXPECT_THROW(Position(0, 5), std::logic_error);
+}
+
+TEST(PositionTest, IntConstructorRowTooHigh) {
+    EXPECT_THROW(Position(11, 5), std::logic_error);
+}
+
+TEST(PositionTest, IntConstructorColTooLow) {
+    EXPECT_THROW(Position(5, 0), std::logic_error);
+}
+
+TEST(PositionTest, IntConstructorColTooHigh) {
+    EXPECT_THROW(Position(5, 11), std::logic_error);
+}
+
+TEST(PositionTest, ValidStringConstructor) {
+    Position pos("(3, 7)");
+    EXPECT_EQ(pos.row(), 3);
+    EXPECT_EQ(pos.col(), 7);
+}
+
+TEST(PositionTest, StringConstructorMinBounds) {
+    Position pos("(1, 1)");
+    EXPECT_EQ(pos.row(), 1);
+    EXPECT_EQ(pos.col(), 1);
+}
+
+TEST(PositionTest, StringConstructorMaxBounds) {
+    Position pos("(10, 10)");
+    EXPECT_EQ(pos.row(), 10);
+    EXPECT_EQ(pos.col(), 10);
+}
+
+TEST(PositionTest, StringConstructorMissingOpenParen) {
+    EXPECT_THROW(Position("3, 7)"), std::logic_error);
+}
+
+TEST(PositionTest, StringConstructorMissingComma) {
+    EXPECT_THROW(Position("(3 7)"), std::logic_error);
+}
+
+TEST(PositionTest, StringConstructorMissingCloseParen) {
+    EXPECT_THROW(Position("(3, 7"), std::logic_error);
+}
+
+TEST(PositionTest, StringConstructorInvalidFormat) {
+    EXPECT_THROW(Position("3, 7"), std::logic_error);
+}
+
+TEST(PositionTest, StringConstructorRowOutOfBounds) {
+    EXPECT_THROW(Position("(11, 5)"), std::logic_error);
+}
+
+TEST(PositionTest, StringConstructorColOutOfBounds) {
+    EXPECT_THROW(Position("(5, 11)"), std::logic_error);
+}
+
+TEST(PositionTest, SetValidRow) {
+    Position pos(5, 5);
+    pos.row(3);
+    EXPECT_EQ(pos.row(), 3);
+}
+
+TEST(PositionTest, SetRowTooLow) {
+    Position pos(5, 5);
+    EXPECT_THROW(pos.row(0), std::logic_error);
+    EXPECT_EQ(pos.row(), 5);
+}
+
+TEST(PositionTest, SetRowTooHigh) {
+    Position pos(5, 5);
+    EXPECT_THROW(pos.row(11), std::logic_error);
+    EXPECT_EQ(pos.row(), 5);
+}
+
+TEST(PositionTest, SetValidCol) {
+    Position pos(5, 5);
+    pos.col(8);
+    EXPECT_EQ(pos.col(), 8);
+}
+
+TEST(PositionTest, SetColTooLow) {
+    Position pos(5, 5);
+    EXPECT_THROW(pos.col(0), std::logic_error);
+    EXPECT_EQ(pos.col(), 5);
+}
+
+TEST(PositionTest, SetColTooHigh) {
+    Position pos(5, 5);
+    EXPECT_THROW(pos.col(11), std::logic_error);
+    EXPECT_EQ(pos.col(), 5);
+}
+
+TEST(PositionTest, ToStringBasic) {
+    Position pos(3, 7);
+    EXPECT_EQ(to_string(pos), "(3, 7)");
+}
+
+TEST(PositionTest, ToStringMinBounds) {
+    Position pos(1, 1);
+    EXPECT_EQ(to_string(pos), "(1, 1)");
+}
+
+TEST(PositionTest, ToStringMaxBounds) {
+    Position pos(10, 10);
+    EXPECT_EQ(to_string(pos), "(10, 10)");
+}
+
+TEST(PositionTest, ParseValidString) {
+    Position pos = parse("(4, 6)");
+    EXPECT_EQ(pos.row(), 4);
+    EXPECT_EQ(pos.col(), 6);
+}
+
+TEST(PositionTest, ParseMinBounds) {
+    Position pos = parse("(1, 1)");
+    EXPECT_EQ(pos.row(), 1);
+    EXPECT_EQ(pos.col(), 1);
+}
+
+TEST(PositionTest, ParseMaxBounds) {
+    Position pos = parse("(10, 10)");
+    EXPECT_EQ(pos.row(), 10);
+    EXPECT_EQ(pos.col(), 10);
+}
+
+TEST(PositionTest, ParseMissingOpenParen) {
+    EXPECT_THROW(parse("4, 6)"), std::logic_error);
+}
+
+TEST(PositionTest, ParseMissingComma) {
+    EXPECT_THROW(parse("(4 6)"), std::logic_error);
+}
+
+TEST(PositionTest, ParseMissingCloseParen) {
+    EXPECT_THROW(parse("(4, 6"), std::logic_error);
+}
+
+TEST(PositionTest, ParseInvalidFormat) {
+    EXPECT_THROW(parse("4, 6"), std::logic_error);
+}
+
+TEST(PositionTest, ParseRowOutOfBounds) {
+    EXPECT_THROW(parse("(11, 5)"), std::logic_error);
+}
+
+TEST(PositionTest, ParseColOutOfBounds) {
+    EXPECT_THROW(parse("(5, 11)"), std::logic_error);
 }
