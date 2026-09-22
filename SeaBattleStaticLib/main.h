be43f382c87d15d13a7b56f46781e55954a50bfc
Новиков Роман <roman141aleksandro2007@gmail.com>
2026-09-22 21:36:03 +0300

    Добавлено полное объявление классов Position, Ship, Gamefield

--- SeaBattleStaticLib/main.h
+++ SeaBattleStaticLib/main.h
@@ -1,1 +1,126 @@
 #pragma once
+#include <stdexcept>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <cctype>
+
+const int Position::_max_row = 10;
+const int Position::_max_col = 10;
+
+class Position {
+private:
+    int _row;
+    int _col;
+
+    static const int _max_row;
+    static const int _max_col;
+
+public:
+    Position() : _row(1), _col(1) {}
+    Position(const Position& other) : _row(other._row), _col(other._col) {}
+    Position(int row, int col);
+    Position(const std::string& str);
+
+    int row() const noexcept { return _row; }
+    int col() const noexcept { return _col; }
+    void row(int new_row);
+    void col(int new_col);
+
+    friend std::string to_string(const Position& pos);
+    friend Position parse(const std::string& str);
+    friend class Ship;
+};
+
+
+enum Direction { Horizontal, Vertical }; // положение корабля на карте
+
+class Ship {
+private:
+    int _size; //Размер корабля 1-4 ячеек
+    Position _coord; //Координата хвоста корабля
+    Direction _direction; //Положение: горизонтальное или вертикальное
+public:
+    Ship() = delete;
+    Ship(int size, int row, char col) : Ship(size, 'H', row, col) {};
+    Ship(int size, Position coord, Direction dir);
+    Ship(int size, Position coord) : Ship(size, coord, Horizontal) {}
+    Ship(int size, char dir, int row, char col);
+    Ship(const Ship& other) = delete;
+    bool is_collision(int size, const Position& coord, Direction dir) const noexcept;
+    void rotate();
+
+    int size() const noexcept { return _size; }
+    Direction direction() const noexcept { return _direction; }
+    int row() const noexcept { return _coord.row(); }
+    int col() const noexcept { return _coord.col(); }
+};
+
+
+
+class GameField {
+private:
+    char** _field;
+    const int _n;
+    const int _m;
+
+    void check_dimensions(int n, int m) const;
+
+    
+    void check_position(int row, char col) const;
+    
+
+    // выделение памяти и заполнение поля
+    void allocate_and_fill(char fill_char = ' ');
+    
+
+    // глубокое копирование из другого объекта
+    void copy_from(const GameField& other);
+    
+
+    // Освобождение памяти
+    void destroy() noexcept {
+        for (int i = 0; i < _n; ++i) {
+            delete[] _field[i];
+        }
+        delete[] _field;
+    }
+
+public:
+    
+    GameField() : _n(10), _m(10) {
+        allocate_and_fill(' ');
+    }
+
+    GameField(int n, int m) : _n(n), _m(m) {
+        check_dimensions(n, m);
+        allocate_and_fill(' ');
+    }
+
+    GameField(char** field, int n, int m);
+    
+    GameField(const GameField& other) : _n(other._n), _m(other._m) {
+        copy_from(other);
+    }
+
+    ~GameField() {
+        destroy();
+    }
+
+
+    void set(int row, char col) {
+        check_position(row, col);
+        _field[row - 1][col - 'A'] = '*';
+    }
+
+
+    char get(int row, char col) const {
+        check_position(row, col);
+        return _field[row - 1][col - 'A'];
+    }
+
+
+    friend std::string to_string(const GameField& gf);
+};
+
+
